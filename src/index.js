e3117bf614eadbaeb9198fa03af3b367b091ade8
lalebdi <loai.alebdi@gmail.com>
2020-08-23 17:48:23 -0400

    setup the store

--- src/index.js
+++ src/index.js
@@ -2,11 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/App';
 import * as serviceWorker from './serviceWorker';
+import { Provider } from 'react-redux';
+import store from './store/index';
 
 ReactDOM.render(
-  <React.StrictMode>
+  <Provider store={store}>
     <App />
-  </React.StrictMode>,
+  </Provider>,
   document.getElementById('root')
 );
 
