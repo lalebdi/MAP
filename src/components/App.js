b56425fef62d89915c816396495d3ec868122e74
lalebdi <loai.alebdi@gmail.com>
2020-08-24 11:01:32 -0400

    looped through the lists

--- src/components/App.js
+++ src/components/App.js
@@ -1,14 +1,18 @@
-import React from 'react';
+import React, { Component} from 'react';
 import MapList from './MapList';
 import { connect } from 'react-redux';
 
-function App() {
+class App extends Component {
+  render() {
+
+    const { lists } = this.props;
   return (
     <div className="App">
       <h1> testing testing</h1>
-      <MapList title="test" />
+      {lists.map(list => (<MapList title={list.title} cards={list.cards} /> ))}
     </div>
   );
+  }
 }
 
 const mapStateToProps = state => ({
