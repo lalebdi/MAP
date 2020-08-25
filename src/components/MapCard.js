5efb00ed02efbb5834e983cbd5e6045bf24fa9b6
lalebdi <loai.alebdi@gmail.com>
2020-08-25 09:48:32 -0400

    implemented the styled components on the cards

--- src/components/MapCard.js
+++ src/components/MapCard.js
@@ -3,18 +3,23 @@ import Card from '@material-ui/core/Card';
 import Typography from '@material-ui/core/Typography';
 import CardContent from '@material-ui/core/CardContent';
 import { Draggable } from 'react-beautiful-dnd';
+import styled from 'styled-components';
+
+const CardContainer = styled.div`
+    margin-bottom: 8px;
+`
 
 const MapCard = ({ text, id, index }) => {
     return (
         <Draggable draggableId={String(id)} index={index}  >
         {provided => (
-            <div ref={provided.innerRef} {...provided.draggableProps} {...provided.dragHandleProps}>
-            <Card style={styles.cardContainer} >
+            <CardContainer ref={provided.innerRef} {...provided.draggableProps} {...provided.dragHandleProps}>
+            <Card>
             <CardContent>
             <Typography gutterBottom> {text} </Typography>
             </CardContent>
         </Card>
-        </div>
+        </CardContainer>
         )}
         </Draggable>
     )
